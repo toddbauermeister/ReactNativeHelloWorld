4f87ad9c9e7b7404139eb34c47627c6637f59650
Todd Bauermeister <toddbauermeister@gmail.com>
2021-07-23 17:08:45 +0200

    make app a functional component

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default App = () => {
   return (
     <View style={styles.container}>
       <Text>Hello World 🎊</Text>
